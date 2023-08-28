41119d4520d1bc62ebdb881b5b0aeeda0ffb9eb9
ritamghoshgds <142888509+ritamghoshgds@users.noreply.github.com>
2023-08-28 16:18:13 +0530

    Update snowflake_connector.py for error handling with snowflake sql

--- snowflake_connector.py
+++ snowflake_connector.py
@@ -1,6 +1,7 @@
 import snowflake.connector
 import pandas as pd
 
+
 def fetch_and_display_data(query):
     snowflake_user = "MitadruChakraborty"
     snowflake_password = "Abcd1234@"
@@ -8,7 +9,7 @@ def fetch_and_display_data(query):
     snowflake_warehouse = "LLM_WH"
     snowflake_database = "LLM_CHATBOT"
     snowflake_schema = "DATA"
-    
+
     conn = snowflake.connector.connect(
         user=snowflake_user,
         password=snowflake_password,
@@ -18,25 +19,37 @@ def fetch_and_display_data(query):
         schema=snowflake_schema,
         role="LLM_RL"
     )
-    
+
     cursor = conn.cursor()
-    
+
     try:
         if query.strip() == '':
             raise ValueError("Query is empty")
-        
-        cursor.execute(query)
+
+        # Wrap the query in a TRY...CATCH block for error handling within Snowflake SQL
+        error_handling_query = f"""
+                BEGIN
+                    TRY
+                        {query}
+                    CATCH (error_var VARIANT)
+                        RETURN PARSE_JSON('{{"error_message": "An error occurred"}}');
+                    END TRY;
+                END;
+            """
+
+        cursor.execute(error_handling_query)
         data = cursor.fetchall()
         columns = [desc[0] for desc in cursor.description]
         df = pd.DataFrame(data, columns=columns)
         return df
-    
+
     except snowflake.connector.errors.ProgrammingError as e:
         return f"Error executing query: {e}"
-    
+
     except Exception as e:
         return f"An error occurred: {e}"
-    
+
+
     finally:
         cursor.close()
         conn.close()
